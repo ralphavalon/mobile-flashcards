f1e1c463e8576fc87084a0dd0d60dded788dbad0
Ralph Avalon <amoedo.raphael@gmail.com>
2018-09-18 08:31:43 -0300

    Add question

--- src/modules/storage/DeckStorage.js
+++ src/modules/storage/DeckStorage.js
@@ -29,3 +29,12 @@ export const createDeck = (name) => {
         resolve(decks);
     })
 }
+
+export const addQuestionToDeck = (question, deck) => {
+    const deckToAdd = decks.find(d => d.id === deck.id)
+    deckToAdd.questions.push(question)
+    decks = decks.filter((d) => d.id !== deck.id).concat([deckToAdd]);
+    return new Promise(function (resolve, reject) {
+        resolve(decks);
+    })
+}
--- src/pages/AddQuestion.js
+++ src/pages/AddQuestion.js
@@ -1,5 +1,6 @@
 import { Container, Content, Form, Text, Button, Icon } from 'native-base';
 import React from 'react';
+import * as storage from '../modules/storage/DeckStorage'
 import FormField from '../components/FormField';
 import HeaderNavigationAware from '../components/HeaderNavigationAware';
 
@@ -10,18 +11,42 @@ export default class AddQuestion extends React.Component {
         )
     });
 
+    state = {
+        question: '',
+        answer: ''
+    }
+
+    toDeck(deck) {
+        this.props.navigation.navigate('ViewDeck', { deck: deck })
+    }
+
     render() {
+        const { question, answer } = this.state
+        const deck = this.props.navigation.getParam('deck')
+
         return (
             <Container style={{ alignItems: 'center' }}>
                 <Content style={{ paddingTop: 20 }}>
                     <Form>
-                        <FormField label="Question" inputName="question" />
-                        <FormField label="Answer" inputName="answer" />
+                        <FormField label="Question" inputName="question" value={question} onChange={(e) => this.setState({ "question": e })} />
+                        <FormField label="Answer" inputName="answer" value={answer} onChange={(e) => this.setState({ "answer": e })} />
                     </Form>
                 </Content>
 
                 <Container>
-                    <Button iconLeft onPress={() => this.props.navigation.navigate('ViewDeck', { 'deck': '' })}>
+                    <Button iconLeft onPress={(e) => {
+                        if (!!question && !!question.trim() && !!answer && !!answer.trim()) {
+                            storage.addQuestionToDeck({
+                                "question": question,
+                                "answer": answer
+                            }, deck)
+                                .then(() => this.toDeck(deck))
+                                .catch(() => Alert.alert('Internal error', 'Cannot create deck'))
+                        } else {
+                            Alert.alert('Validation error', 'Name must not be empty');
+                        }
+
+                    }}>
                         <Icon name='ios-add-circle' />
                         <Text>Add Question</Text>
                     </Button>
